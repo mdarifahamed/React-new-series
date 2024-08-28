5c9035fc3d2adc229bc46f810ebe7d69046209f2
Arif <arifonly4@gmail.com>
2024-08-29 01:17:44 +0530

    cardfirstapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Footer from "./Footer";
+import { Header } from "./Header";
+import { Col, Row, Container, Card, Button } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Container fluid>
+        <Container>
+          <Row>
+            <Col className="col-12 text-center py-4">
+              <h1>Our Course</h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>course1</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>course1</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>course1</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col lg="3" md="6">
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>course1</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            
+          </Row>
+        </Container>
+      </Container>
+
+      {/* <div className='container'>
+        <p className='text-danger'>welcome to home page</p>
+      <div className='para'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
+      </div> */}
+
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+// function Card(){
+//   return(
+//     <div className='carditem'>card div</div>
+//   )
+// }
---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Nav } from "react-bootstrap";
+
+function Header() {
+  return (
+    <>
+  <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">About</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">course</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+    </>
+   
+  )
+}
+
+export {Header}
+
